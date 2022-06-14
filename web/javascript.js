618f1e6ef7ea29b6f32ae1d2eb8fd810ec1380a4
Morten Carlsson <mortencarlsson@Mortens-MBP.localdomain>
2022-06-14 13:46:26 +0200

    First running example

--- web/javascript.js
+++ web/javascript.js
@@ -1,6 +1,6 @@
 (async function () {
 
-
+let sha256 = new WasmSHA256();
 
 	let difficulty = 5;
 
@@ -12,13 +12,13 @@
 	}
 
 	function toHexString(byteArray) {
-		return Array.from(byteArray, function(byte) {
+		return Array.from(byteArray, function (byte) {
 			return ('0' + (byte & 0xFF).toString(16)).slice(-2);
 		}).join('')
 	}
 
 	class Blockchain {
-		constructor( initialBlock ) {
+		constructor(initialBlock) {
 			// Create our genesis block
 			this.chain = [initialBlock];
 		}
@@ -52,8 +52,14 @@
 	}
 
 	// Our hash function.
-	async function getHash( block ) {
+	async function getHash(block) {
 		const hash = await digestMessage(block.prevHash + block.timestamp + JSON.stringify(block.data) + block.nonce);
+		return hash
+	}
+
+	// Our hash function.
+	async function getHashWS(block) {
+		const hash = await sha256(block.prevHash + block.timestamp + JSON.stringify(block.data) + block.nonce);
 		return hash
 	}
 
@@ -79,34 +85,74 @@
 		}
 	}
 
-	window.doBlockChainTest = async function ( inDifficulty, callback ){
+	window.doBlockChainTest = async function (inDifficulty, callback) {
 
 		difficulty = inDifficulty
 
-		console.time( "Timing" )
+		console.time("Timing")
 
 		const initialBlock = new Block(Date.now().toString())
 		initialBlock.hash = await getHash(initialBlock)
 		const theChain = new Blockchain(initialBlock);
 
-// Add a new block
-		const block = new Block(Date.now().toString(), {from: "John", to: "Bob", amount: 100})
+		// Add a new block
+		const block = new Block(Date.now().toString(), { from: "John", to: "Bob", amount: 100 })
 		block.hash = await getHash(block);
-		console.debug( "javascript.js:87", block )
+		console.debug("javascript.js:87", block)
 
 		await theChain.addBlock(block);
 
-// (This is just a fun example, real cryptocurrencies often have some more steps to implement).
+		// (This is just a fun example, real cryptocurrencies often have some more steps to implement).
 
-// Prints out the updated chain
+		// Prints out the updated chain
 
 		console.log(theChain.chain);
-		console.debug( "javascript.js:97", "Difficulty:", difficulty )
-		console.debug( "javascript.js:97", theChain.chain[1].timestamp - theChain.chain[0].timestamp )
+		console.debug("javascript.js:97", "Difficulty:", difficulty)
+		console.debug("javascript.js:97", theChain.chain[1].timestamp - theChain.chain[0].timestamp)
 
-		console.timeEnd( "Timing" )
+		console.timeEnd("Timing")
 
-		callback( theChain )
+		callback(theChain)
 	}
 
+	window.doBlockChainTestWS = async function (inDifficulty, callback) {
+		
+		difficulty = inDifficulty
+
+		console.time("Timing")
+
+		const initialBlock = new Block(Date.now().toString())
+		initialBlock.hash = sha256.hashString(initialBlock)
+		const theChain = new Blockchain(initialBlock);
+
+		// Add a new block
+		const block = new Block(Date.now().toString(), { from: "John", to: "Bob", amount: 100 })
+		block.hash = sha256.hashString(block);
+		console.debug("javascript.js:131", block)
+
+		await theChain.addBlock(block);
+
+		// (This is just a fun example, real cryptocurrencies often have some more steps to implement).
+
+		// Prints out the updated chain
+
+		console.log(theChain.chain);
+		console.debug("javascript.js:140", "Difficulty:", difficulty)
+		console.debug("javascript.js:141", theChain.chain[1].timestamp - theChain.chain[0].timestamp)
+
+		console.timeEnd("Timing")
+
+		callback(theChain)
+	}
+
+	fetch("sha256.wasm?v=1.0.2")
+    .then(res => res.arrayBuffer())
+    .then(buffer => {
+      sha256.loadWasmBuffer(buffer);
+
+      sha256.on("ready", function () {
+		console.log('wasm ready')
+	  });
+    });
 })()
+
