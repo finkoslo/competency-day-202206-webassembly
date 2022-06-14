7d0addfa9c812cc126da9ab4ec528d713a093da4
Andreas Øverland <andreas@fink.no>
2022-06-14 10:58:19 +0200

    Vi har implementert en enkel blockchain-miner i javascript på browseren som vi skal bruke til å sammenlikne ytelsen på native JS vs WebAssembly

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # competency-day-202206-webassembly
 Web-assembly docs, snippets and examples from competency day 202206 
+
+
+## Getting started
+Install local webserver: 
+npm install --global http-server
+
+Run webserver
+http-server -c 1 web
+
+Open page:
+http://127.0.0.1:8080/
---
+++ web/javascript.js
@@ -0,0 +1,104 @@
+(async function () {
+
+
+
+	const difficulty = 5;
+
+	async function digestMessage(message) {
+		const encoder = new TextEncoder();
+		const data = encoder.encode(message);
+		const hash = await crypto.subtle.digest('SHA-256', data);
+		return toHexString(new Uint8Array(hash))
+	}
+
+	function toHexString(byteArray) {
+		return Array.from(byteArray, function(byte) {
+			return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+		}).join('')
+	}
+
+	class Blockchain {
+		constructor( initialBlock ) {
+			// Create our genesis block
+			this.chain = [initialBlock];
+		}
+
+		getLastBlock() {
+			return this.chain[this.chain.length - 1];
+		}
+
+		async addBlock(block) {
+			block.prevHash = this.getLastBlock().hash;
+			block.hash = await getHash(block);
+			await block.mine(difficulty);
+			this.chain.push(Object.freeze(block));
+		}
+
+
+		async isValid(blockchain = this) {
+			// Iterate over the chain, we need to set i to 1 because there are nothing before the genesis block, so we start at the second block.
+			for (let i = 1; i < blockchain.chain.length; i++) {
+				const currentBlock = blockchain.chain[i];
+				const prevBlock = blockchain.chain[i - 1];
+
+				// Check validation
+				if (currentBlock.hash !== await getHash(currentBlock) || prevBlock.hash !== currentBlock.prevHash) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	// Our hash function.
+	async function getHash( block ) {
+		const hash = await digestMessage(block.prevHash + block.timestamp + JSON.stringify(block.data) + block.nonce);
+		return hash
+	}
+
+	class Block {
+		constructor(timestamp = "", data = []) {
+			this.timestamp = timestamp;
+			this.data = data;
+			this.hash = ""
+			this.prevHash = ""; // previous block's hash
+			this.nonce = 0;
+		}
+
+
+		async mine(difficulty) {
+			// Basically, it loops until our hash starts with
+			// the string 0...000 with length of <difficulty>.
+			while (!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
+				// We increases our nonce so that we can get a whole different hash.
+				this.nonce++;
+				// Update our new hash with the new nonce value.
+				this.hash = await getHash(this);
+			}
+		}
+	}
+
+	console.time( "Timing" )
+
+	const initialBlock = new Block(Date.now().toString())
+	initialBlock.hash = await getHash(initialBlock)
+	const JeChain = new Blockchain(initialBlock);
+
+// Add a new block
+	const block = new Block(Date.now().toString(), {from: "John", to: "Bob", amount: 100})
+	block.hash = await getHash(block);
+	console.debug( "javascript.js:87", block )
+
+	await JeChain.addBlock(block);
+
+// (This is just a fun example, real cryptocurrencies often have some more steps to implement).
+
+// Prints out the updated chain
+	console.log(JeChain.chain);
+	console.debug( "javascript.js:97", "Difficulty:", difficulty )
+	console.debug( "javascript.js:97", JeChain.chain[1].timestamp - JeChain.chain[0].timestamp )
+
+	console.timeEnd( "Timing" )
+
+})()
